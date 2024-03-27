2c43e5fddca2241abc6aafd430b701858e120d59
Srikaran Devarakonda <114800064+srikaran3004@users.noreply.github.com>
2024-03-27 11:20:58 +0530

    Update open-ai.js

--- config/open-ai.js
+++ config/open-ai.js
@@ -1,18 +1,10 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { OpenAI } from "openai";
 import dotenv from 'dotenv';
-import { setDefaultAutoSelectFamily } from 'net';
 
 dotenv.config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
 });
 
-const openai = new OpenAIApi(configuration);
-
 export default openai;
-
-
-
-
-
